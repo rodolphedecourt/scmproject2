b45dcc7c278ebd7f492af52f47d12ec47b8002c6
UsTimon@gmail.com <UsTimon@gmail.com@207e9a88-1412-6e39-2143-a7c6724f2ecb>
2010-12-05 16:00:12 +0000

    LabVIEW SLAM added, without MathScript

--- trunk/Slam/SLAM_run.m
+++ trunk/Slam/SLAM_run.m
@@ -1,1 +1,8 @@
-  [Xout, Pout, Xodometry, measuredLandmarks] = SLAM(X, P, dX, measurements);
+X=[0;0;0];
+P=[0.01 0 0;0 0.01 0;0 0 0.01];
+dX=[0.1;0.1;-0.003];
+measurements=[0.89 -0.18];
+[Xout, Pout, Xodometry, measuredLandmarks] = SLAM(X, P, dX, measurements);
+Xout
+Pout
+Xodometry
